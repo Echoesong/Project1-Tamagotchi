495f6b4dc7ce6e6f639855125c41d78bccb4fed0
Joseph Felker <jayefelker@gmail.com>
2023-03-07 09:40:04 -0600

    Set default state vars and cached DOM elements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 console.log("Scripts read")
 
+
+// <-----Default global variables----->
+
+let tamagotchi
+let meters = {
+    hunger: 'hunger string',
+    happiness: 'happiness string',
+    health: 'health string',
+}
+let victory = {}
+
+// Put a string in tamagotchi and victory if you want to display them in console 
+// console.log(tamagotchi, meters, victory)
+
+// <-----Cached DOM elements----->
+
+const titleEl = document.querySelector('h1')
+const displayEl = document.querySelector('h2')
+const buttonEls = document.querySelectorAll('button')
+
+console.log(titleEl, displayEl, buttonEls)
