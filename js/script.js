49ee6d41aacec17f2e8b5f7fd52483942711c040
Joseph Felker <jayefelker@gmail.com>
2023-03-07 11:06:37 -0600

    connected new div elements to buttonEls var in script file

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
-console.log("Scripts read")
-
+console.log("Scripts read");
 
 // <-----Default global variables----->
 
-let tamagotchi
+let tamagotchi;
 let meters = {
-    hunger: 'hunger string',
-    happiness: 'happiness string',
-    health: 'health string',
-}
-let victory = {}
+  hunger: "hunger string",
+  happiness: "happiness string",
+  health: "health string",
+};
+let victory = {};
 
-// Put a string in tamagotchi and victory if you want to display them in console 
+// Put a string in tamagotchi and victory if you want to display them in console
 // console.log(tamagotchi, meters, victory)
 
 // <-----Cached DOM elements----->
 
-const titleEl = document.querySelector('h1')
-const displayEl = document.querySelector('h2')
-const buttonEls = document.querySelectorAll('button')
+const titleEl = document.querySelector("h1");
+const displayEl = document.querySelector("h2");
+const buttonEls = document.querySelectorAll("div"); //If you are going to manipulate this, turn it into an array
 
 console.log(titleEl, displayEl, buttonEls)
+
+// <-----Functions----->
+
+function init(){
+    console.log("Initializing...")
+}
+
+init()
